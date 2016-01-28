16e9b63daffa02200937d96837ce2dc657079373
agangal <ashish.gangal1990@gmail.com>
2016-01-27 22:35:08 -0800

    Go and Stop pause animation work

--- TimeTracker/Timer.xaml.cs
+++ TimeTracker/Timer.xaml.cs
@@ -33,11 +33,13 @@ namespace TimeTracker
         private void ellipse_Tapped(object sender, TappedRoutedEventArgs e)
         {
             Storyboard myStoryboard;
-            Debug.WriteLine("In Tapped Event");
+            Debug.WriteLine("In GoTapped Event");
             myStoryboard = (Storyboard)this.Resources["GoButtonTapped"];
             myStoryboard.Begin();
             GoEllipse.IsTapEnabled = false;
+            StopEllipse.IsTapEnabled = true;
             
+            StopTextBlock.IsTapEnabled = true;
         }
 
         private void PauseEllipse_Tapped(object sender, TappedRoutedEventArgs e)
@@ -46,6 +48,43 @@ namespace TimeTracker
         }
 
         private void StopEllipse_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Storyboard myStoryboard;
+            Debug.WriteLine("In StopTapped Event");
+            myStoryboard = (Storyboard)this.Resources["StopButtonPress"];
+            myStoryboard.Begin();
+            GoEllipse.IsTapEnabled = true;
+            StopEllipse.IsTapEnabled = false;
+            StopGrid.IsTapEnabled = false;
+            StopTextBlock.IsTapEnabled = false;
+
+        }
+
+        private void StopTextBlock_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Storyboard myStoryboard;
+            Debug.WriteLine("In Stop Text Tapped Event");
+            myStoryboard = (Storyboard)this.Resources["StopButtonPress"];
+            myStoryboard.Begin();
+            GoEllipse.IsTapEnabled = true;
+            StopEllipse.IsTapEnabled = false;
+            StopGrid.IsTapEnabled = false;
+            StopTextBlock.IsTapEnabled = false;
+        }
+
+        private void StopGrid_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Storyboard myStoryboard;
+            Debug.WriteLine("In Stop Grid Tapped Event");
+            myStoryboard = (Storyboard)this.Resources["StopButtonPress"];
+            myStoryboard.Begin();
+            GoEllipse.IsTapEnabled = true;
+            StopEllipse.IsTapEnabled = false;
+            StopGrid.IsTapEnabled = false;
+            StopTextBlock.IsTapEnabled = false;
+        }
+
+        private void PauseGrid_Tapped(object sender, TappedRoutedEventArgs e)
         {
 
         }
